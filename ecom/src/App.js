7eec5772a9748f9a26a1b2be0db3ee177e3589ec
Wenhao Tian <wtian16@bu.edu>
2024-01-23 16:39:38 -0800

    created comps

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,8 +1,14 @@
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <main>
+        <h1>Welcome Here!</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
