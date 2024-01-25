100154d96c836ca9415d93d00a8ee478b7c5a04b
Wenhao Tian <wtian16@bu.edu>
2024-01-25 13:41:52 -0800

    setup homepage

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,14 +1,14 @@
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import { Container } from 'react-bootstrap'
-
+import HomePage from './Pages/HomePage';
 function App() {
   return (
     <div className="App">
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome Here!</h1>
+          <HomePage />
         </Container>
 
       </main>
