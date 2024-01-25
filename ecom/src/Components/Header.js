dae04eab10448b4801df8e7f43c5dad543449395
Wenhao Tian <wtian16@bu.edu>
2024-01-25 12:28:24 -0800

    setup header

--- ecom/src/Components/Header.js
+++ ecom/src/Components/Header.js
@@ -9,16 +9,18 @@ function Header() {
         <header>
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand href="/">My Shop</Navbar.Brand>
+                    <Navbar.Brand href="/" className='col-3'>My Shop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                    <div className='col-6'></div>
+                    <Navbar.Collapse id="basic-navbar-nav" className='col-3'>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/home"><i className='fa-solid fa-house-chimney'></i> Home</Nav.Link>
+                            <Nav.Link href="/cart"><i class="fa-solid fa-cart-shopping"></i> Cart</Nav.Link>
+                            <Nav.Link href="/cart"><i class="fa-regular fa-user"></i> Login</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">
-                                    Another action
+                                    Another actionß
                                 </NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
