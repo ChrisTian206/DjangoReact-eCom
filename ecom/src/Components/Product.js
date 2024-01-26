85a2e5ee5d53ab55325be5172f927b3924b2eabc
Wenhao Tian <wtian16@bu.edu>
2024-01-26 12:08:18 -0800

    update product card

--- ecom/src/Components/Product.js
+++ ecom/src/Components/Product.js
@@ -4,10 +4,28 @@ import { Card } from 'react-bootstrap'
 function Product({ product }) {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href=''>
-                <Card.Img />
+            <a href={`/product/${product._id}`}>
+                <Card.Img src={product.image} />
             </a>
 
+            <Card.Body>
+                <a href={`product/${product._id}`}>
+                    <Card.Title as='div'>
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+
+                    <Card.Text as='div'>
+                        <div className='my-3'>
+                            {product.rating} from {product.numReviews} reviews
+                        </div>
+                    </Card.Text>
+
+                    <Card.Text as='h3'>
+                        ${product.price}
+                    </Card.Text>
+
+                </a>
+            </Card.Body>
         </Card>
     )
 }
