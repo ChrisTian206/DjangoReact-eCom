b8087b78e1f9e22cda91b4cdcf9e0537977b225f
Wenhao Tian <wtian16@bu.edu>
2024-01-30 21:24:04 -0800

    reactify header links

--- ecom/src/Components/Header.js
+++ ecom/src/Components/Header.js
@@ -3,30 +3,36 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
     return (
         <header>
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand href="/" className='col-3'>My Shop</Navbar.Brand>
+                    {/* Using LinkContainer reactifies link instead of refresh into home page */}
+                    <LinkContainer to='/'>
+                        <Navbar.Brand className='col-3'>My Shop</Navbar.Brand>
+                    </LinkContainer>
+
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <div className='col-6'></div>
                     <Navbar.Collapse id="basic-navbar-nav" className='col-3'>
                         <Nav className="mr-auto">
-                            <Nav.Link href="/home"><i className='fa-solid fa-house-chimney'></i> Home</Nav.Link>
-                            <Nav.Link href="/cart"><i class="fa-solid fa-cart-shopping"></i> Cart</Nav.Link>
-                            <Nav.Link href="/cart"><i class="fa-regular fa-user"></i> Login</Nav.Link>
+                            <LinkContainer to='/'>
+                                <Nav.Link ><i className='fa-solid fa-house-chimney'></i>Home</Nav.Link>
+                            </LinkContainer>
+
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i class="fa-solid fa-cart-shopping"></i>Cart</Nav.Link>
+                            </LinkContainer>
+
+                            <LinkContainer to='/login'>
+                                <Nav.Link ><i class="fa-regular fa-user"></i>Login</Nav.Link>
+                            </LinkContainer>
+
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another actionß
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
