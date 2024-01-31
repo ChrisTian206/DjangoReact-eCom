163f4179982853b1542b4c8a0b55038f7007f91a
Wenhao Tian <wtian16@bu.edu>
2024-01-30 21:15:36 -0800

    added routings

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -2,18 +2,23 @@ import Header from './Components/Header'
 import Footer from './Components/Footer'
 import { Container } from 'react-bootstrap'
 import HomePage from './Pages/HomePage';
+import ProductPage from './Pages/ProductPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomePage />
+          <Routes>
+            <Route path='/' Component={HomePage} exact />
+            <Route path='/product/:id' Component={ProductPage} />
+          </Routes>
         </Container>
-
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
