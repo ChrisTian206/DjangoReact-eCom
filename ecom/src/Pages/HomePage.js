f0449f6d02d98dcdefc5ffed16a878a93d0bcc0a
Wenhao Tian <wtian16@bu.edu>
2024-02-05 13:11:01 -0800

    setup FE axios

--- ecom/src/Pages/HomePage.js
+++ ecom/src/Pages/HomePage.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import products from '../products'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../Components/Product'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/'
 
 function HomePage() {
-
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            setProducts((await axios.get('api/products')).data);
+        }
+        fetchProducts();
+    }, [])
     return (
         <div>
             <h1>New Products</h1>
