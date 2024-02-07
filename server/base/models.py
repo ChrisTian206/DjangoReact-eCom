b57d01508592db10b5870c6a886dc00d0da20240
Wenhao Tian <wtian16@bu.edu>
2024-02-07 14:00:15 -0800

    allowed image upload

--- server/base/models.py
+++ server/base/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete = models.SET_NULL, null=True)
     name = models.CharField(max_length = 200, null=True, blank=True)
-    #image = 
+    image = models.ImageField(null=True, blank=True)
     brand = models.CharField(max_length = 200, null=True, blank=True)
     category = models.CharField(max_length = 200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -55,7 +55,7 @@ class OrderItem(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2,null=True, blank=True)
-    #image =
+    image = models.CharField(max_length=200, null=True, blank=True)
     _id =  models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
