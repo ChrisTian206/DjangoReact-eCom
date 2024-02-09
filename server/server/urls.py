4676ab9878264540eb9143ae3378cdc3abc99ae3
Wenhao Tian <wtian16@bu.edu>
2024-02-09 12:32:22 -0800

    added static routes to imgs

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,7 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from base import urls as baseUrls
 
+
+#those two are from the myenv where django source code is included
+#those allows us to connect our urls 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(baseUrls)),
+    path('api/', include(baseUrls)), 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
