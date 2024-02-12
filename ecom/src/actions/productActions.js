d8acdbaf7ec17d502235080f949c9dbfa96ff139
Wenhao Tian <wtian16@bu.edu>
2024-02-12 14:45:06 -0800

    added actions

--- ecom/src/actions/productActions.js
+++ ecom/src/actions/productActions.js
@@ -1,6 +1,25 @@
+import axios from 'axios'
 import {
     PRODUCT_LIST_REQUESTED,
     PRODUCT_LIST_FAILED,
     PRODUCT_LIST_SUCCESS
 } from '../constants/productConstants'
 
+const listProducts = () => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_LIST_REQUESTED })
+
+        const { data } = await axios.get(`api/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_LIST_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_LIST_FAILED,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message : error.message,
+        })
+    }
+}
