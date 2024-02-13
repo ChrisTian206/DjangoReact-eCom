1c9c3455bcf6066bc1058b0e8d43a14759f4243f
Wenhao Tian <wtian16@bu.edu>
2024-02-13 10:37:44 -0800

    rework with redux

--- ecom/src/Pages/HomePage.js
+++ ecom/src/Pages/HomePage.js
@@ -3,17 +3,31 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../Components/Product'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+//useSelector used for selecting specific part of state from redux
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/'
 
 function HomePage() {
-    const [products, setProducts] = useState([]);
+    //using Redux action, dont need local state anymore 
+    //const [products, setProducts] = useState([]);
+
+    const dispatch = useDispatch()
+
+    //reference store.js for what's in the state
+    const productList = useSelector(state => state.productList)
     useEffect(() => {
-        const fetchProducts = async () => {
-            setProducts((await axios.get('api/products/')).data);
-        }
-        fetchProducts();
+        //data fetching now handled in Action
+        // const fetchProducts = async () => {
+        //     setProducts((await axios.get('api/products/')).data);
+        // }
+        // fetchProducts();
+
+        dispatch(listProducts())
     }, [])
+
+    const products = [];
 
     return (
         <div>
