62424751ae6d1376ca95a80392dc09f2c61a9c7b
Wenhao Tian <wtian16@bu.edu>
2024-02-14 11:33:52 -0800

    CENTERED IT!

--- ecom/src/Components/Loader.js
+++ ecom/src/Components/Loader.js
@@ -3,17 +3,19 @@ import { Spinner } from 'react-bootstrap'
 
 function loader() {
     return (
-        <Spinner animation='border' role='status'
-            style={{
-                height: '100px',
-                width: '100px',
-                margin: 'auto',
-                display: 'block'
-            }}>
+        <div style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+        }}>
+            <Spinner animation='border' role='status' style={{ height: '100px', width: '100px' }}>
 
-            <span className='sr-only'>Loading...</span>
-        </Spinner>
-    )
+                {/* sr-only means screen reader only, you won't see it on screen thou */}
+                <span className='sr-only'>Loading...</span>
+            </Spinner>
+        </div>
+    );
 }
 
 export default loader
