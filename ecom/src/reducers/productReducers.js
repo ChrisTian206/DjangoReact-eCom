a9b276db11d7e743ee1710ae949348bc3e07d3ba
Wenhao Tian <wtian16@bu.edu>
2024-02-14 12:43:55 -0800

    product details reducer

--- ecom/src/reducers/productReducers.js
+++ ecom/src/reducers/productReducers.js
@@ -1,7 +1,10 @@
 import {
     PRODUCT_LIST_REQUESTED,
     PRODUCT_LIST_FAILED,
-    PRODUCT_LIST_SUCCESS
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_DETAILS_REQUESTED,
+    PRODUCT_DETAILS_FAILED,
+    PRODUCT_DETAILS_SUCCESS,
 } from '../constants/productConstants'
 
 export const productListReducers = (state = { products: [] }, action) => {
@@ -11,6 +14,20 @@ export const productListReducers = (state = { products: [] }, action) => {
         case PRODUCT_LIST_SUCCESS:
             return { loading: false, products: action.payload }
         case PRODUCT_LIST_FAILED:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+export const productDetailsReducers = (state = { product: { reviews: [] } }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUESTED:
+            return { loading: true, ...state }
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload }
+        case PRODUCT_DETAILS_FAILED:
             return { loading: false, error: action.payload }
 
         default:
