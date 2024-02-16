618c52e51c3b75053253e974771aa750704d526c
Wenhao Tian <wtian16@bu.edu>
2024-02-16 10:34:02 -0800

    product detail actions

--- ecom/src/actions/productActions.js
+++ ecom/src/actions/productActions.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 import {
     PRODUCT_LIST_REQUESTED,
     PRODUCT_LIST_FAILED,
-    PRODUCT_LIST_SUCCESS
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_DETAILS_REQUESTED,
+    PRODUCT_DETAILS_FAILED,
+    PRODUCT_DETAILS_SUCCESS,
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
@@ -18,6 +21,25 @@ export const listProducts = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAILED,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message : error.message,
+        })
+    }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUESTED })
+
+        const { data } = await axios.get(`api/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAILED,
             payload: error.response && error.response.data.message ?
                 error.response.data.message : error.message,
         })
