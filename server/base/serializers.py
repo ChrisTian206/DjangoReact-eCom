1283af13db565b94442b1678b7c56f9ca311a8ee
Wenhao Tian <wtian16@bu.edu>
2024-02-29 10:32:19 -0800

    setup user serializer

--- server/base/serializers.py
+++ server/base/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
+
+#this User is one of the built-in User model, it comes with authentication and authorization
 from django.contrib.auth.models import User
 from .models import Product
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','email']
 
 #what it does is it's gonna wrap around the Product model and turn it into JSON format
 class ProductSerializer(serializers.ModelSerializer):
