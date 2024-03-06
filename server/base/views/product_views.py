ba9a8a68adf48cb6fe8474dd115d093906578645
Wenhao Tian <wtian16@bu.edu>
2024-03-06 15:01:36 -0800

    minor fix

--- server/base/views/product_views.py
+++ server/base/views/product_views.py
@@ -5,11 +5,12 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 # Create your views here.
 from django.contrib.auth.models import User
-from .models import Product
-from .products import products
+from base.models import Product
+from base.products import products
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from base.serializers import ProductSerializer
 
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
