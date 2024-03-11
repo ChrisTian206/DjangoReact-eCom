b29369f916c559e3771fced115f65cda824e1b83
Wenhao Tian <wtian16@bu.edu>
2024-03-11 15:07:52 -0700

    initial setup

---
+++ ecom/src/Pages/LoginPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Button, Form } from 'react-bootstrap'
+import loader from '../Components/Loader'
+import Messager from '../Components/Messager'
+
+function LoginPage() {
+    return (
+        <div>LoginPage</div>
+    )
+}
+
+export default LoginPage
