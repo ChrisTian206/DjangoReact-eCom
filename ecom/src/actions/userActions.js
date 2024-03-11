07bbec6d38d2f7a82a6707f75cbcf39347c848db
Wenhao Tian <wtian16@bu.edu>
2024-03-11 14:09:45 -0700

    added login action

---
+++ ecom/src/actions/userActions.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from '../constants/userConstants'
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+
+
+
+        const { data } = axios.post(
+            '/api/users/login/',
+            { 'username': email, 'password': password }, //username -> email, ikr, it's how I setup in Django
+            {
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload: error.response && error.response.data.detail ?
+                error.response.data.detail : error.message,
+        })
+    }
+}
