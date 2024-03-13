a595631d95dac1b500b55c6df0ba11885914b32c
Wenhao Tian <wtian16@bu.edu>
2024-03-12 17:34:31 -0700

    add useSelector and logout

--- ecom/src/Components/Header.js
+++ ecom/src/Components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom';
 
 function Header() {
+    const userState = useSelector(state => state.userLogin)
+    const { userInfo } = userState
+
+    const logoutHandler = () => {
+        console.log('Log out now')
+    }
     return (
         <header>
             <Navbar expand="lg" className="bg-body-tertiary">
@@ -30,9 +38,20 @@ function Header() {
                                 <Nav.Link><i class="fa-solid fa-cart-shopping"></i>Cart</Nav.Link>
                             </LinkContainer>
 
-                            <LinkContainer to='/login'>
-                                <Nav.Link ><i class="fa-regular fa-user"></i>Login</Nav.Link>
-                            </LinkContainer>
+                            {
+                                userInfo ?
+                                    <NavDropdown title={`${userInfo.username}`}>
+                                        <LinkContainer to={'/profile'}>
+                                            <Nav.Link><i class="fa-solid fa-user"></i>Profile</Nav.Link>
+                                        </LinkContainer>
+
+                                        <Nav.Link onClick={logoutHandler}><i class="fa-solid fa-door-open"></i>Logout</Nav.Link>
+                                    </NavDropdown> :
+                                    <LinkContainer to='/login'>
+                                        <Nav.Link ><i class="fa-regular fa-user"></i>Login</Nav.Link>
+                                    </LinkContainer>
+                            }
+
 
                             {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
