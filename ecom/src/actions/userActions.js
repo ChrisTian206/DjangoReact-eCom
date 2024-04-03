f8a720db24de92c43d250731fafb86ba6f5359d6
Wenhao Tian <wtian16@bu.edu>
2024-04-03 15:18:43 -0700

    step4 add actions

--- ecom/src/actions/userActions.js
+++ ecom/src/actions/userActions.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import {
     USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT,
     USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS,
-    USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS
+    USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_RESET, USER_UPDATE_PROFILE_SUCCESS,
 } from '../constants/userConstants'
 
 
@@ -110,6 +111,49 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail ?
+                error.response.data.detail : error.message,
+        })
+    }
+}
+
+export const updateProfile = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_PROFILE_REQUEST
+        })
+
+        const {
+            userLogin: { userInfo }
+        } = getState()
+
+        const { data } = await axios.put(
+            `api/users/profile/update`,
+            user,
+            {
+                headers: {
+                    'content-type': 'application/json',
+                    Authorization: `Bearer ${userInfo.token}`
+                }
+            }
+        )
+
+        dispatch({
+            type: USER_UPDATE_PROFILE_SUCCESS,
+            payload: data,
+        })
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data,
+        })
+
+        //also updates local storage
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+        dispatch({
+            type: USER_UPDATE_PROFILE_FAIL,
             payload: error.response && error.response.data.detail ?
                 error.response.data.detail : error.message,
         })
