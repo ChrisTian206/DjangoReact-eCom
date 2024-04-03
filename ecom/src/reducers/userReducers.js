3b2e36d2bcaa76bfcc28c1ef0f3a35f2a03619a6
Wenhao Tian <wtian16@bu.edu>
2024-04-03 15:16:48 -0700

    step2 add reducer

--- ecom/src/reducers/userReducers.js
+++ ecom/src/reducers/userReducers.js
@@ -1,7 +1,8 @@
 import {
     USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT,
     USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS,
-    USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS
+    USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_RESET, USER_UPDATE_PROFILE_SUCCESS
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -40,6 +41,24 @@ export const userRegisterReducer = (state = {}, action) => {
 }
 
 export const userDetailsReducer = (state = { user: {} }, action) => {
+
+    switch (action.type) {
+        case USER_UPDATE_PROFILE_REQUEST:
+            return { loading: true }
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, userInfo: action.payload, success: true }
+        case USER_UPDATE_PROFILE_FAIL:
+            return { loading: false, error: action.payload }
+
+        case USER_UPDATE_PROFILE_RESET:
+            return {}
+        default:
+            return state
+    }
+}
+
+
+export const userUpdateProfileReducer = (state = {}, action) => {
 
     switch (action.type) {
         case USER_DETAILS_REQUEST:
