3076d20cbbe434c16ab720d4e369f238a7e1ab5f
Wenhao Tian <wtian16@bu.edu>
2024-04-06 23:31:15 -0700

    bug fix

--- ecom/src/actions/userActions.js
+++ ecom/src/actions/userActions.js
@@ -3,7 +3,7 @@ import {
     USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT,
     USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS,
     USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS,
-    USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_RESET, USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_RESET, USER_UPDATE_PROFILE_SUCCESS, USER_DETAILS_RESET,
 } from '../constants/userConstants'
 
 
@@ -22,7 +22,7 @@ export const login = (email, password) => async (dispatch) => {
                 }
             })
 
-        //console.log('log in data: ', data)
+        console.log('userAction > login > api/users/login/ data: ', data)
 
         dispatch({
             type: USER_LOGIN_SUCCESS,
@@ -45,12 +45,15 @@ export const logout = () => (dispatch) => {
     dispatch({
         type: USER_LOGOUT
     })
+    dispatch({
+        type: USER_DETAILS_RESET
+    })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
     try {
         dispatch({
-            type: USER_LOGIN_REQUEST
+            type: USER_REGISTER_REQUEST
         })
 
         const { data } = await axios.post(
@@ -139,6 +142,8 @@ export const updateProfile = (user) => async (dispatch, getState) => {
                 }
             }
         )
+
+        console.log('userAction, after sent update PUT req, the response is: ', data)
 
         dispatch({
             type: USER_UPDATE_PROFILE_SUCCESS,
